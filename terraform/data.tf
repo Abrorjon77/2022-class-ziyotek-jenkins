43b12f1dba8cbbef0d0e5c7f6cd5a8398dadfc7c
Abrorjon77 <110739859+Abrorjon77@users.noreply.github.com>
2022-12-23 18:09:24 -0500

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,18 +1,11 @@
 data "aws_ami" "example" {
   most_recent = true
-  owners      = ["816725581106"]
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20211001.1-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-*x86_64-gp2"]
   }
 }
 
 
-data "aws_region" "current" {}
-
-output "image_id" {
-  value = data.aws_ami.example.image_id
-}
-
-data "aws_caller_identity" "current" {}
