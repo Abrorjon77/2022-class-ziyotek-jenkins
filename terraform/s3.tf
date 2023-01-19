f92ded7f6f78f98b30aef32bad574c7fd01717c1
Abrorjon77 <110739859+Abrorjon77@users.noreply.github.com>
2023-01-19 10:57:09 -0500

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "dev_bucket" {
-  bucket = "jenkins-bucket-ziyotek-${data.aws_caller_identity.current.account_id}"
+  bucket = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
   
  
 
