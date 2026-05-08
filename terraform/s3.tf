ec0a5c30800fe40a4f11ff5453d710533899daf7
abror <abroor@yahoo.com>
2026-05-08 08:51:54 -0400

    upgrade

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,39 +1,38 @@
 resource "aws_s3_bucket" "dev_bucket" {
-  bucket = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
-  
- 
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-              "arn:aws:s3:::${aws_s3_bucket.dev_bucket.id}/*"
-          ]
-       }
-    ]
-}
-EOF
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-
-  }
+  bucket        = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_object" "dev" {
-  key          = "index.html"
+resource "aws_s3_bucket_policy" "dev_bucket" {
+  bucket = aws_s3_bucket.dev_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = ["s3:GetObject"]
+      Resource  = ["arn:aws:s3:::${aws_s3_bucket.dev_bucket.id}/*"]
+    }]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "dev_bucket" {
+  bucket = aws_s3_bucket.dev_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "dev" {
   bucket       = aws_s3_bucket.dev_bucket.id
+  key          = "index.html"
   content      = file("../assets/index.html")
   content_type = "text/html"
-
 }
